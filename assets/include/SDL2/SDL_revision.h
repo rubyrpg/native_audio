562116f091910db4e40ee997e1229daa4e9e910d
Max Hatfull <max.h@beam.org>
2026-01-29 22:38:59 +0000

    Update sdl

--- assets/include/SDL2/SDL_revision.h
+++ assets/include/SDL2/SDL_revision.h
@@ -1,6 +1,8 @@
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION SDL_VENDOR_INFO
-#else
-#define SDL_REVISION ""
-#endif
+/* #undef SDL_VENDOR_INFO */
 #define SDL_REVISION_NUMBER 0
+
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "SDL-release-2.32.10-0-g5d2495703 (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "SDL-release-2.32.10-0-g5d2495703"
+#endif
