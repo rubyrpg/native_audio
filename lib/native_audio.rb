9dc4411165fa2a7b57f67c23dc9c729ec03dc303
Max Hatfull <max.h@beam.org>
2026-01-29 21:44:34 +0000

    comment everythign out

--- lib/native_audio.rb
+++ lib/native_audio.rb
@@ -1,61 +1,61 @@
-# frozen_string_literal: true
-
-require_relative './audio'
-puts "require done"
-sleep 1
-puts "slept, initing"
-Audio.init
-puts "init done"
-sleep 1
-puts "slept, cleaning up"
-Audio.cleanup
-puts "cleanup done"
-sleep 1
-puts "slept, exiting"
-
-module NativeAudio
-  class Clip
-    attr_reader :clip
-
-    def initialize(path)
-      @path = path
-      @clip = Audio.load(path)
-    end
-  end
-
-  class AudioSource
-    def initialize(clip)
-      @clip = clip
-      @channel = AudioSource.channels.count
-      AudioSource.channels << self
-    end
-
-    def play
-      Audio.play(@channel, @clip.clip)
-    end
-
-    def stop
-      Audio.stop(@channel)
-    end
-
-    def pause
-      Audio.pause(@channel)
-    end
-
-    def resume
-      Audio.resume(@channel)
-    end
-
-    def set_pos(angle, distance)
-      Audio.set_pos(@channel, angle, distance)
-    end
-
-    def set_volume(volume)
-      Audio.set_volume(@channel, volume)
-    end
-
-    def self.channels
-      @channels ||= []
-    end
-  end
-end
+# # frozen_string_literal: true
+#
+# require_relative './audio'
+# puts "require done"
+# sleep 1
+# puts "slept, initing"
+# Audio.init
+# puts "init done"
+# sleep 1
+# puts "slept, cleaning up"
+# Audio.cleanup
+# puts "cleanup done"
+# sleep 1
+# puts "slept, exiting"
+#
+# module NativeAudio
+#   class Clip
+#     attr_reader :clip
+#
+#     def initialize(path)
+#       @path = path
+#       @clip = Audio.load(path)
+#     end
+#   end
+#
+#   class AudioSource
+#     def initialize(clip)
+#       @clip = clip
+#       @channel = AudioSource.channels.count
+#       AudioSource.channels << self
+#     end
+#
+#     def play
+#       Audio.play(@channel, @clip.clip)
+#     end
+#
+#     def stop
+#       Audio.stop(@channel)
+#     end
+#
+#     def pause
+#       Audio.pause(@channel)
+#     end
+#
+#     def resume
+#       Audio.resume(@channel)
+#     end
+#
+#     def set_pos(angle, distance)
+#       Audio.set_pos(@channel, angle, distance)
+#     end
+#
+#     def set_volume(volume)
+#       Audio.set_volume(@channel, volume)
+#     end
+#
+#     def self.channels
+#       @channels ||= []
+#     end
+#   end
+# end
