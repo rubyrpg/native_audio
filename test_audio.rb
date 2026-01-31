e2731f4340227d9e44a97f0d57078247653026e2
Max Hatfull <max.h@beam.org>
2026-01-31 19:22:53 +0000

    Fixup build pipeline

--- test_audio.rb
+++ test_audio.rb
@@ -8,15 +8,17 @@ clip = NativeAudio::Clip.new('boom.wav')
 source1 = NativeAudio::AudioSource.new(clip)
 source2 = NativeAudio::AudioSource.new(clip)
 
-puts "Playing first boom (left)..."
+puts "Playing first boom (left, low pitch)..."
 source1.play
 source1.set_pos(270, 255)  # left
+source1.set_pitch(0.5)     # low
 
 sleep 3
 
-puts "Playing second boom (right)..."
+puts "Playing second boom (right, high pitch)..."
 source2.play
 source2.set_pos(90, 255)   # right
+source2.set_pitch(2.0)     # high
 
 # Wait for both to finish
 sleep clip.duration
