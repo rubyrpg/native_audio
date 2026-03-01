6023c1f59453230b8ec663b6a49828c7082751b4
Max Hatfull <max.h@beam.org>
2026-03-01 12:42:48 +0000

    add guard and bump

--- lib/native_audio.rb
+++ lib/native_audio.rb
@@ -69,6 +69,7 @@ module NativeAudio
     end
 
     def stop
+      return unless @channel
       NativeAudio.audio_driver.stop(@channel)
       self.class.owners.delete(@channel)
       @channel = nil
@@ -90,40 +91,40 @@ module NativeAudio
     end
 
     def pause
-      NativeAudio.audio_driver.pause(@channel)
+      NativeAudio.audio_driver.pause(@channel) if @channel
     end
 
     def resume
-      NativeAudio.audio_driver.resume(@channel)
+      NativeAudio.audio_driver.resume(@channel) if @channel
     end
 
     def set_pos(angle, distance)
       @params[:pos] = [angle, distance]
-      NativeAudio.audio_driver.set_pos(@channel, angle, distance)
+      NativeAudio.audio_driver.set_pos(@channel, angle, distance) if @channel
     end
 
     def set_pan(pan)
       @params[:pan] = pan
-      NativeAudio.audio_driver.set_pan(@channel, pan)
+      NativeAudio.audio_driver.set_pan(@channel, pan) if @channel
     end
 
     def seek(seconds)
-      NativeAudio.audio_driver.seek(@channel, seconds)
+      NativeAudio.audio_driver.seek(@channel, seconds) if @channel
     end
 
     def set_volume(volume)
       @params[:volume] = volume
-      NativeAudio.audio_driver.set_volume(@channel, volume)
+      NativeAudio.audio_driver.set_volume(@channel, volume) if @channel
     end
 
     def set_pitch(pitch)
       @params[:pitch] = pitch
-      NativeAudio.audio_driver.set_pitch(@channel, pitch)
+      NativeAudio.audio_driver.set_pitch(@channel, pitch) if @channel
     end
 
     def set_looping(looping)
       @params[:looping] = looping
-      NativeAudio.audio_driver.set_looping(@channel, looping)
+      NativeAudio.audio_driver.set_looping(@channel, looping) if @channel
     end
 
     def add_delay_tap(time_ms:, volume:)
@@ -135,16 +136,18 @@ module NativeAudio
 
     def enable_reverb(enabled = true)
       @params[:reverb_enabled] = enabled
-      NativeAudio.audio_driver.enable_reverb(@channel, enabled)
+      NativeAudio.audio_driver.enable_reverb(@channel, enabled) if @channel
     end
 
     def set_reverb(room_size: 0.5, damping: 0.3, wet: 0.3, dry: 1.0)
       @params[:reverb] = { room_size: room_size, damping: damping, wet: wet, dry: dry }
-      NativeAudio.audio_driver.enable_reverb(@channel, true)
-      NativeAudio.audio_driver.set_reverb_room_size(@channel, room_size)
-      NativeAudio.audio_driver.set_reverb_damping(@channel, damping)
-      NativeAudio.audio_driver.set_reverb_wet(@channel, wet)
-      NativeAudio.audio_driver.set_reverb_dry(@channel, dry)
+      if @channel
+        NativeAudio.audio_driver.enable_reverb(@channel, true)
+        NativeAudio.audio_driver.set_reverb_room_size(@channel, room_size)
+        NativeAudio.audio_driver.set_reverb_damping(@channel, damping)
+        NativeAudio.audio_driver.set_reverb_wet(@channel, wet)
+        NativeAudio.audio_driver.set_reverb_dry(@channel, dry)
+      end
     end
 
     def delay_taps
--- native_audio.gemspec
+++ native_audio.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |spec|
   spec.name          = 'native_audio'
-  spec.version       = '0.5.7'
+  spec.version       = '0.5.8'
   spec.authors       = ['Max Hatfull']
   spec.email         = ['max.hatfull@gmail.com']
   spec.summary       = 'A simple audio library for Ruby'
